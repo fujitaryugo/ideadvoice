16737ebf222dc632e1b7ab3ab54f4c9304e865e2
fujitaryugo <ryu5.1m3s1k@gmail.com>
2019-07-10 06:45:52 +0000

    add controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+  	case resource
+  	when User
+  	 user_path(current_user)
+  	when Client
+  	 client_path(current_client)
+  	end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/update'
   devise_for :users
   devise_for :clients
   devise_for :admins
   root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :update]
 end
